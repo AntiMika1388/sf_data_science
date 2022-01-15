9e5c1dbb86c94fb6fbf9597b84b0f2301759b16c
AntiMika1388 <antimika1388@yandex.ru>
2022-01-15 17:43:41 +0700

    v1

--- project_0/game.py
+++ project_0/game.py
@@ -1,22 +1,53 @@
-"""Игра угадай число"""
+
+
 
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
 
-# количество попыток
-count = 0
+def binary_search(number=1) -> int:
+    """Реализация алгоритма инарного поиска
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+    Returns:
+        int: Число попыток
+    """
+    counter = 0
+    left = 0 # нижняя граница промежутка
+    right = 100 # верхняя граница промежутка
 
-while True:
-    count+=1
-    predict_number = int(input("Угадай число от 1 до 100: "))
-    
-    if predict_number > number:
-        print("Число должно быть меньше!")
+    while left <= right:
+        counter+= 1
+        predict_number = (left + right)/2 # поиск среднего значения в промежутке
+        
+        if number < predict_number:
+            right = predict_number - 1 # изменение верхней границы, если число больше загаданного    
+        if number > predict_number:
+            left= predict_number + 1 # изменение нижней границы, если число меньше загаданного   
+        if number == predict_number:
+            break  # завершение цикла при условии что числа равны
+        
+    return counter
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
-    
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break #конец игры выход из цикла
+
+def score_game(binary_search) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+    Args:
+        random_predict ([type]): функция угадывания
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size = (1000))  # загадали список чисел
+
+    for numb in random_array:
+        count_ls.append(binary_search(numb))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(binary_search)
